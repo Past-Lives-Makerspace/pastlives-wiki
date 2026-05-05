58773103966285916315d99e76f6808cae3992be
stvncx <189052183+stvncx@users.noreply.github.com>
2026-05-04 21:48:28 -0700

    Fix missing semicolon in require_once

--- LocalSettings.php
+++ LocalSettings.php
@@ -148,4 +148,4 @@ wfLoadExtension( 'WikiEditor' );
 
 # End of automatically generated settings.
 # Add more configuration options below.
-require_once __DIR__ . '/pl-extras.php'
+require_once __DIR__ . '/pl-extras.php';
